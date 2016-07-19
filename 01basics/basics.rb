dc1a2e11ac75d872458c1efec385d4014d28c02c
Rebecca Thaden <rebecca@thadenphoto.com>
2016-07-19 16:08:58 -0500

    Add first project files

---
+++ 01basics/basics.rb
@@ -0,0 +1,63 @@
+#1
+puts "Rebecca " + "Thaden"
+
+puts
+
+#2
+
+puts 6789 / 1000
+
+puts 2378 % 1000 / 100
+
+puts 2346 % 100 / 10
+
+puts 8558 % 10
+
+puts
+
+#3
+movies = { back_to_the_future: 1985,
+           fargo: 1996,
+           memento: 2000,
+           titanic: 1997,
+           concussion: 2015}
+
+puts movies [:back_to_the_future]
+puts movies [:fargo]
+puts movies [:memento]
+puts movies [:titanic]
+puts movies [:concussion]
+
+puts
+
+#4
+dates = [1985, 1996, 200, 1997, 2015]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+puts dates[4]
+
+puts
+
+#5
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+puts 
+
+#6
+puts 2.4 * 2.4
+puts 5.625 * 5.625
+puts 42.3 * 42.3
+
+puts 
+
+#7
+puts "The error message is a result of not using a closing curly brace."
+
+
+
